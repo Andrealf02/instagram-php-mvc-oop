87d8538ed12f92d8b695e7aa8db3afc345f5e4f7
Andrea_f <web.andrea.02@gmail.com>
2022-07-04 17:33:23 +0200

    ULTIMO archivo lib

--- src/model/lib/utilsImage.php
+++ src/model/lib/utilsImage.php
@@ -10,5 +10,22 @@ class UtilImage{
         $hash = md5(Date('Ymdgi') . $filename). '.' . $ext;
         $targer_file = $target_dir . $hash;
         $uploadOk = 1;
+
+        if(check === false){
+            $uploadOk = 1 ;
+            
+        }else{
+            $uploadOk = 0;
+        }
+        
+        if($uploadOk == 0) {
+            throw new ExceptioN('NO SE SUBIO LA IMG');
+        }else{
+            if(move_uploaded_file($photo["tmp_name"], $target_file)){
+                return $hash;
+            }else{
+                return "";
+            }
+        }
     }
 }
