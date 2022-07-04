a70b5d58a7666945dde44d46b6a635866060d553
Andrea_f <web.andrea.02@gmail.com>
2022-07-04 17:41:04 +0200

    controller singup, nuevo user

--- src/model/lib/controller.php
+++ src/model/lib/controller.php
@@ -4,22 +4,22 @@ namespace Andrea\instagram\lib;
 class Controller{
     
     private View $view;
-    public function __construct(){
+    protected function __construct(){
         $this->view = new View();
     }
 
-    public function render(string $name, array $data = []){
+    protected function render(string $name, array $data = []){
         $this->view->render($name, $data);
     }
 
-    public function post(string $param){
+    protected function post(string $param){
         if(!isset($_POST[$param])){
             return NULL;
         }
         return $_POST[$param];
     }
 
-    public function get(string $param){
+    protected function get(string $param){
         if(!isset($_GET[$param])){
             return NULL;
         }
---
+++ src/model/lib/controller/singupcontroller.php
@@ -0,0 +1,26 @@
+<?php
+namespace Andrea\instagram\lib;
+
+class SingUp extends Controller{
+    public function __construct(){
+        parent::__construct();
+    }
+    public function registrer(){
+        $username = $this->post('username');
+        $pass = $this->post('pass');
+        $profile = $this->file('profile');
+        if(
+            !is_null($username)&&
+            !is_null($pass)&&
+            !is_null($profile)
+        ){
+            $pictureName = UtilImage:: storeImage($profile);
+            $user = new User($username, $pass);
+            $user-> setProfile($pictureName);
+            $user-> save;
+            header('location : /instagram/login');
+        }else{
+            this->render('error/index');
+        }
+    }
+}
